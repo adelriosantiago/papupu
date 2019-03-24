b9b4a578966aacc9fbba56d2ae27f6d3199b0f0c
Alejandro del Río <adelriosantiago@gmail.com>
2019-03-23 18:32:57 -0600

    Add bin file

--- cli.js
+++ cli.js
@@ -1,12 +1,15 @@
-var cmd = require('node-cmd');
+#!/usr/bin/env node
+
+const cmd = require('node-cmd');
+const [,, ...args] = process.argv;
 
 const commandToRun = `cd ${ process.cwd() } && npm version patch && npm publish && git push`;
 
 console.log("Running: ", commandToRun);
 
-cmd.get(
+/*cmd.get(
   commandToRun,
   function(err, data, stderr) {
     console.log("Done: ", err, data, stderr);
   }
-);
+);*/
