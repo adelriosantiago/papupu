080361d880896f23dc4d783174aa2e3568528166
Alejandro Santiago <adelriosantiago@gmail.com>
2026-01-07 00:30:33 -0500

    Add art

--- cli.js
+++ cli.js
@@ -4,7 +4,16 @@ const cmd = require("node-cmd");
 const [, , ...args] = process.argv;
 const commandToRun = `cd ${process.cwd()} && npm version patch && npm publish && git push`;
 
-console.log("Running: ", commandToRun);
+// Print "PaPuPu" art
+console.info(`
+██████╗  █████╗ ██████╗ ██╗   ██╗██████╗ ██╗   ██╗
+██╔══██╗██╔══██╗██╔══██╗██║   ██║██╔══██╗██║   ██║
+██████╔╝███████║██████╔╝██║   ██║██████╔╝██║   ██║
+██╔═══╝ ██╔══██║██╔═══╝ ██║   ██║██╔═══╝ ██║   ██║
+██║     ██║  ██║██║     ╚██████╔╝██║     ╚██████╔╝
+╚═╝     ╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝      ╚═════╝
+Running: ${commandToRun}
+`);
 
 cmd.get(commandToRun, (err, data, stderr) => {
   if (err) {
